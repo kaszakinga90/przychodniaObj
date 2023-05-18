bc9cb967c3df59da2899f59fdb3624b79e0d228a
Karol Marciniak <marciniakarol@gmail.com>
2023-05-18 18:36:12 +0200

    Controller: Przychodnia
    Nagłówke, navbar i stopka.
    Style bootstrap za bardzo nie działają.

---
+++ przychodnia/app/Http/Controllers/Przychodnia.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Przychodnia extends Controller
+{
+    static function naglowek($tytul)
+	{
+		return '
+		<!doctype html>
+		<html lang="en">
+			<head>
+			  <meta charset="utf-8">
+			  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+			  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+			  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+			  <link href="./style.css" rel="stylesheet">
+			</head>
+
+			<header style="box-shadow: 7px 7px 5px rgba(0, 0, 0, 0.239);">
+
+			  <nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: white;">
+				<div class="container-fluid px-4 py-2 navCont">
+				  <a class="navbar-brand">
+					<h3 class="offcanvas-title ps-4"><i class="bi bi-brightness-high fs-3 pt-0 pe-3"></i>Promyczek</h3>
+				  </a>
+
+				  <!-- user section -->
+				  <div class="btn-group ">
+					<button type="button pt-0" class="btn fs-4 mx-0">
+					  <a class="dropdown-item fs-4" style="color:#5d6778"><i class="bi bi-person-circle pt-0 fs-3 pe-3" style="color:#5d6778"></i>Zaloguj się</a>
+					</button>
+				  </div>
+				</div>
+			  </nav>
+			</header>
+		<body class="bodySettings">';
+	}
+	/*
+	static function menu()
+	{
+		return "<body bgcolor=yellow text='000FFF'>
+		<!-- 1. przycisk ładujący stronę odpowiedzialną za zarządzanie studentami -->
+		<input type=button value=' STUDENCI ' onClick='window.location=\"studenci.php\"'>
+		<br><br>
+		
+		<!-- 2. formularz ładujący stronę odpowiedzialną za zarządzanie ocenami -->
+		<form name=menu action='oceny.php'>
+		<input type=submit value=' OCENY '> 
+		</form>
+		
+		<!-- 3. odsyłacz (link) do strony odpowiedzialnej za zarządzanie przedmiotami -->
+		<a href='przedmioty.php'> PRZEDMIOTY </a>";
+	}
+	*/
+	static function stopka()
+	{
+		return '
+		<footer class="container pb-5">
+		  <p class="ps-5">Designed by Kinga Kasza & Karol Marciniak</p>
+		</footer>
+
+		  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+		 </body>
+		</html>';
+	}
+	
+	
+	public function __invoke(){	//public, żeby działało w Laravelu		
+		return Przychodnia::naglowek("Strona główna") . self::stopka();
+		//return view('main');
+	}
+}
