795c737fab74dec3908ac4789ac49d36d1b42af5
Kinga Kasza <kaszakinga90@gmail.com>
2023-05-26 12:00:50 +0200

    documents base

---
+++ przychodnia/app/Models/Document.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Document extends Model
+{
+    use HasFactory;
+
+    protected $attributes = [
+        'Type',
+        'DoctorId',
+        'PatientId',
+        'IssueDate',
+        'Signature',
+    ];
+
+    // Tabela w BD
+    protected $table = 'document';
+    public $timestamps = false;
+
+    //Metoda zwracająca obiekt Doctor przy wyświetlaniu dokumentow
+    public function doctor() {
+        return $this->belongsTo(Doctor::class, 'DoctorId');
+    }
+    //Metoda zwraca wszystkich lekarzy
+    public function doctors() {
+        return Doctor::all();
+    }
+}
